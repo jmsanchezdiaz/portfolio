2f82412e8c21584b88d96f0ef9f5c5f79dbc3f89
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2021-10-18 01:50:37 -0300

    removed-scrolltop

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,35 +1,27 @@
 import "./Header.scss";
 import Logo from "../../static/projects-imgs/LOGO JM.png";
 import ResponsiveMenu from "./ResponsiveMenu/ResponsiveMenu";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   const [isScrolling, setIsScrolling] = useState(false);
-  const [isScrollingTop, setIsScrollingTop] = useState(false);
-  const lastScroll = useRef(window.scrollY);
 
   useEffect(() => {
     const handleScroll = () => {
       const curScroll = window.scrollY;
 
-      if (curScroll < lastScroll.current) setIsScrollingTop(true);
-      else if (curScroll > lastScroll.current) setIsScrollingTop(false);
-      else if (curScroll === lastScroll.current) setIsScrollingTop(true);
-
       if (curScroll !== 0) setIsScrolling(true);
       else setIsScrolling(false);
-      lastScroll.current = curScroll;
     };
     handleScroll();
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [isScrolling, isScrollingTop, lastScroll]);
+  }, [isScrolling]);
 
   return (
     <header
       style={{
-        visibility: isScrollingTop ? "visible" : "hidden",
-        boxShadow: !isScrolling ? "none" : "0px 1px 2px rgba(0,0,0,0.178)",
+        boxShadow: !isScrolling ? "none" : "0px 1px 2px rgba(0,0,0,0.150)",
       }}
       className="header"
     >
