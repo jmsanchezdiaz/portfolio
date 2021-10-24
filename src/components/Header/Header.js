a7ccaba78c38eaa4da51ae9816649df01e50488d
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2021-10-23 23:29:22 -0300

    modified-components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,28 @@
 import "./Header.scss";
 import Logo from "../../static/projects-imgs/LOGO JM.png";
-import ResponsiveMenu from "./ResponsiveMenu/ResponsiveMenu";
+import BarsMenu from "../../static/icons/bars-menu.svg";
 import { useEffect, useState } from "react";
 
 const Header = () => {
   const [isScrolling, setIsScrolling] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 630) setIsMobileView(true);
+      else setIsMobileView(false);
+      setIsOpen(false);
+    };
+
+    window.addEventListener("load", handleResize);
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("load", handleResize);
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isMobileView]);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -18,6 +36,13 @@ const Header = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, [isScrolling]);
 
+  const closeMenu = () => setIsOpen(false);
+  const openMenu = () => {
+    if (isMobileView) {
+      setIsOpen(!isOpen);
+    }
+  };
+
   return (
     <header
       style={{
@@ -26,6 +51,7 @@ const Header = () => {
       className="header"
     >
       <img src={Logo} alt="logo" />
+      {/* DESKTOP MENU */}
       <nav className="navbar">
         <ul className="navbar__list">
           <li>
@@ -41,8 +67,43 @@ const Header = () => {
             <a href="#Contactame">Contactame</a>
           </li>
         </ul>
-        <ResponsiveMenu />
       </nav>
+      {/* MOBILE MENU */}
+      <ul
+        style={{
+          clipPath: isOpen
+            ? " polygon(0 0, 100% 0, 100% 100%, 0 100%)"
+            : "polygon(0 0, 100% 0, 100% 0, 0 0)",
+        }}
+        className="navbar__list--responsive"
+      >
+        <li>
+          <a onClick={closeMenu} href="#SobreMi">
+            Sobre Mí
+          </a>
+        </li>
+        <li>
+          <a onClick={closeMenu} href="#Tecnologias">
+            Tecnologias
+          </a>
+        </li>
+        <li>
+          <a onClick={closeMenu} href="#Proyectos">
+            Proyectos
+          </a>
+        </li>
+        <li>
+          <a onClick={closeMenu} href="#Contactame">
+            Contactame
+          </a>
+        </li>
+      </ul>
+      <img
+        onClick={openMenu}
+        src={BarsMenu}
+        alt="bars-icon"
+        className="navbar__bars"
+      />
     </header>
   );
 };
