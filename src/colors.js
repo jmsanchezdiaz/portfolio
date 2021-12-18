e440869715718b81fd5f19ec53f4652fcac5cc31
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2021-12-18 01:27:56 -0300

    added new icons

---
+++ src/colors.js
@@ -0,0 +1,8 @@
+export const colors = {
+  sunMainColor: ' #2c3e50',
+  sunBkgColor: '#10171d',
+  sunSecondaryColor: '#ecf0f1',
+  sunThirdColor: '#1abc9c',
+  moonBkgColor: '#222B33',
+  moonMainColor: '#28A9FF',
+};
