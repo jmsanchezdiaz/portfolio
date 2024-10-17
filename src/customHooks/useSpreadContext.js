b0b359fb0b680e8b563d5098441fa2e1ba5c6b46
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2024-10-17 02:03:19 -0300

    Updates

--- src/customHooks/useSpreadContext.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { DarkModeContext } from '../Context/DarkModeContext';
-
-const useSpreadContext = () => {
-  const context = useContext(DarkModeContext);
-  return { ...context };
-};
-
-export { useSpreadContext };
