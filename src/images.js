8fe72fefcf04259cf547ae65bba4948fdf9f8778
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2024-10-17 01:48:08 -0300

    Update version

--- src/images.js
+++ src/images.js
@@ -8,101 +8,104 @@ import {
   NextJS,
   ChakraUI,
   Typescript,
-  Bootstrap
-} from './icons';
-import RestApp from './static/projects-imgs/RestCountriesApp.jpg';
-import FotoLinkedin from './static/projects-imgs/FotoLinkedIn.jpg';
-import Gipharg from './static/projects-imgs/GiphargImg.png';
-import Aerolab from './static/projects-imgs/AerolabImg.jpg';
-import BlogPersonal from './static/projects-imgs/blog-personal.png';
-import MeliDetails from './static/projects-imgs/mercadolibre.png';
-import AlkemyChallenge from './static/projects-imgs/AlkemyChallenge.png';
+  Bootstrap,
+  Cypress,
+  MUI,
+  Rails,
+  Ruby,
+  Git,
+  Jest,
+  GraphQL
+} from "./icons";
+import RestApp from "./static/projects-imgs/RestCountriesApp.jpg";
+import FotoLinkedin from "./static/projects-imgs/FotoLinkedIn.webp";
+import Gipharg from "./static/projects-imgs/GiphargImg.png";
+import Aerolab from "./static/projects-imgs/AerolabImg.jpg";
+import BlogPersonal from "./static/projects-imgs/blog-personal.png";
+import MeliDetails from "./static/projects-imgs/mercadolibre.png";
+import AlkemyChallenge from "./static/projects-imgs/AlkemyChallenge.png";
 
 export const technologies = [
-  { src: Html, alt: 'html photo', label: 'HTML' },
-  { src: Css3, alt: 'css photo', label: 'CSS3' },
-  { src: Sass, alt: 'sass photo', label: 'SASS' },
-  { src: Javascript, alt: 'javascript photo', label: 'JAVASCRIPT' },
-  { src: Typescript, alt: 'typescript photo', label: 'TYPESCRIPT' },
-  { src: ReactJS, alt: 'react js photo', label: 'REACT.JS' },
-  { src: Postgres, alt: 'postgres photo', label: 'POSTGRES SQL' },
+  { src: Html, alt: "html photo", label: "HTML" },
+  { src: Css3, alt: "css photo", label: "CSS3" },
+  { src: Sass, alt: "sass photo", label: "SASS" },
+  { src: Javascript, alt: "javascript photo", label: "JAVASCRIPT" },
+  { src: Typescript, alt: "typescript photo", label: "TYPESCRIPT" },
+  { src: ReactJS, alt: "react js photo", label: "REACT.JS" },
+  { src: Postgres, alt: "postgres photo", label: "POSTGRES SQL" },
   {
-    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg',
-    alt: 'jest photo',
-    label: 'JEST'
+    src: Jest,
+    alt: "jest photo",
+    label: "JEST"
   },
   {
-    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
-    alt: 'git photo',
-    label: 'GIT'
+    src: Git,
+    alt: "git photo",
+    label: "GIT"
   },
-  {
-    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg',
-    alt: 'firebase photo',
-    label: 'FIREBASE'
-  },
+
   {
     src: NextJS,
-    label: 'NEXT.JS',
-    alt: 'next js photo'
+    label: "NEXT.JS",
+    alt: "next js photo"
   },
-  {
-    src: ChakraUI,
-    label: 'CHAKRA UI',
-    alt: 'Chakra ui photo'
-  },
-  { src: Bootstrap, label: 'BOOTSTRAP', alt: 'Bootstrap photo' }
+
+  { src: Cypress, label: "CYPRESS", alt: "Cypress photo" },
+  { src: MUI, label: "MATERIAL UI", alt: "Material ui photo" },
+  { src: Ruby, label: "RUBY", alt: "Ruby photo" },
+  { src: Rails, label: "RAILS", alt: "Rails photo" },
+  { src: GraphQL, label: "GraphQL", alt: "GraphQL photo" }
 ];
 
 export const projects = [
   {
     src: RestApp,
-    repoName: 'rest-countries-app',
-    alt: 'rest countries app project',
-    url: 'https://jmsanchezdiaz.github.io/rest-countries-app',
-    projectName: 'REST COUNTRIES APP',
+    repoName: "rest-countries-app",
+    alt: "rest countries app project",
+    url: "https://jmsanchezdiaz.github.io/rest-countries-app",
+    projectName: "REST COUNTRIES APP",
     technologiesImgs: [ReactJS, Typescript, Sass]
   },
   {
-    projectName: 'HEROES APP ALKEMY',
+    projectName: "HEROES APP ALKEMY",
     src: AlkemyChallenge,
-    repoName: 'heroes-app-alkemy',
-    url: 'https://jmsanchezdiaz.github.io/heroes-app-alkemy',
-    alt: 'alkemy heroes app project',
+    repoName: "heroes-app-alkemy",
+    url: "https://jmsanchezdiaz.github.io/heroes-app-alkemy",
+    alt: "alkemy heroes app project",
     technologiesImgs: [ReactJS, Typescript, Bootstrap]
   },
   {
-    projectName: 'GIPHARG',
+    projectName: "GIPHARG",
     src: Gipharg,
-    repoName: 'gipharg',
-    url: 'https://jmsanchezdiaz.github.io/gipharg',
+    repoName: "gipharg",
+    url: "https://jmsanchezdiaz.github.io/gipharg",
     technologiesImgs: [ReactJS, Typescript, Sass],
-    alt: 'giphy app project'
+    alt: "giphy app project"
   },
   {
     src: Aerolab,
-    projectName: 'AEROLAB',
-    repoName: 'aerolab-challenge',
-    url: 'https://jmsanchezdiaz.github.io/aerolab-challenge',
-    alt: 'products ecommerce aerolab project',
+    projectName: "AEROLAB",
+    repoName: "aerolab-challenge",
+    url: "https://jmsanchezdiaz.github.io/aerolab-challenge",
+    alt: "products ecommerce aerolab project",
     technologiesImgs: [ReactJS, Typescript, Sass]
   },
 
   {
     src: BlogPersonal,
-    projectName: 'BLOG PERSONAL',
-    repoName: 'aerolab-challenge',
-    url: 'https://my-blog-personal.vercel.app/',
-    alt: 'products ecommerce aerolab project',
+    projectName: "BLOG PERSONAL",
+    repoName: "aerolab-challenge",
+    url: "https://my-blog-personal.vercel.app/",
+    alt: "products ecommerce aerolab project",
     technologiesImgs: [NextJS, Javascript, ChakraUI]
   },
   {
     src: MeliDetails,
-    projectName: 'Mercado Libre Challenge',
-    repoName: 'meli-details-challenge',
-    url: 'https://meli-details-challenge.vercel.app/productId',
-    alt: 'challenge de pagina de producto mercado libre',
+    projectName: "Mercado Libre Challenge",
+    repoName: "meli-details-challenge",
+    url: "https://meli-details-challenge.vercel.app/productId",
+    alt: "challenge de pagina de producto mercado libre",
     technologiesImgs: [ReactJS, Typescript, ChakraUI]
   }
 ];
-export const photo = { src: FotoLinkedin, alt: 'foto de mi persona' };
+export const photo = { src: FotoLinkedin, alt: "foto de mi persona" };
